f21bf9dd8fb2afbe9cd27409c3d235244d717fd1
Max Xu <max@graphpaper.co>
2015-11-20 05:54:19 -0800

    update

--- client/lib/graphiq.js
+++ client/lib/graphiq.js
@@ -2,40 +2,40 @@ graphiq = [];
 
 graphiq.push({
     title: 'Facebook Inc. (FB)',
-    embed: '<div style="width:100%;margin:10px 0;"><iframe src="https://w.graphiq.com/w/232XrYdU9xP" width="700" height="516" frameborder="0" scrolling="no" style="position:static;vertical-align:top;margin:0 auto;display:block;width:700px !important;max-width:100%;min-height:516px;"></iframe><div style="text-align:center;"><a target="_blank" href="http://listings.findthecompany.com/l/14143267/Facebook-Inc-in-Menlo-Park-CA" style="font:14px/16px arial;color:#3d3d3d;">Facebook Inc. (FB) | FindTheCompany</a></div></div>'
+    embed: '<iframe seamless src="https://w.graphiq.com/w/232XrYdU9xP" width="700" height="516" frameborder="0" scrolling="no" style="position:static;vertical-align:top;margin:0 auto;display:block;width:700px !important;max-width:100%;min-height:516px;"></iframe >'
 })
 
 graphiq.push({
     title: 'Facebook Inc. (FB) Quarterly Revenue',
-    embed: '<div style="width:100%;margin:10px 0;"><iframe src="https://w.graphiq.com/w/ai3jxJ8t0CV" width="650" height="487" frameborder="0" scrolling="no" style="position:static;vertical-align:top;margin:0 auto;display:block;width:650px !important;max-width:100%;min-height:487px;"></iframe><div style="text-align:center;"><a target="_blank" href="http://listings.findthecompany.com/l/14143267/Facebook-Inc-in-Menlo-Park-CA" style="font:14px/16px arial;color:#3d3d3d;">Facebook Inc. (FB) Quarterly Revenue | FindTheCompany</a></div></div>'
+    embed: '<iframe seamless src="https://w.graphiq.com/w/ai3jxJ8t0CV" width="650" height="487" frameborder="0" scrolling="no" style="position:static;vertical-align:top;margin:0 auto;display:block;width:650px !important;max-width:100%;min-height:487px;"></iframe >'
 })
 
 graphiq.push({
     title: 'Facebook Inc.\'s Timeline',
-    embed: '<div style="width:100%;margin:10px 0;"><iframe src="https://w.graphiq.com/w/95wYeNV3g8t" width="725" height="677" frameborder="0" scrolling="no" style="position:static;vertical-align:top;margin:0 auto;display:block;width:725px !important;max-width:100%;min-height:677px;"></iframe><div style="text-align:center;"><a target="_blank" href="http://listings.findthecompany.com/l/14143267/Facebook-Inc-in-Menlo-Park-CA" style="font:14px/16px arial;color:#3d3d3d;">Facebook Inc.\'s Timeline | FindTheCompany</a></div></div>'
+    embed: '<iframe seamless src="https://w.graphiq.com/w/95wYeNV3g8t" width="725" height="677" frameborder="0" scrolling="no" style="position:static;vertical-align:top;margin:0 auto;display:block;width:725px !important;max-width:100%;min-height:677px;"></iframe>'
 })
 
 graphiq.push({
     title: 'Facebook Inc.\'s Acquisitions (Since 2006)',
-    embed: '<div style="width:100%;margin:10px 0;"><iframe src="https://w.graphiq.com/w/eAcxDjc5Pwx" width="860" height="433" frameborder="0" scrolling="no" style="position:static;vertical-align:top;margin:0 auto;display:block;width:860px !important;max-width:100%;min-height:433px;"></iframe><div style="text-align:center;"><a target="_blank" href="http://listings.findthecompany.com/l/14143267/Facebook-Inc-in-Menlo-Park-CA" style="font:14px/16px arial;color:#3d3d3d;">Facebook Inc.\'s Acquisitions (Since 2006) | FindTheCompany</a></div></div>'
+    embed: '<iframe seamless src="https://w.graphiq.com/w/eAcxDjc5Pwx" width="860" height="433" frameborder="0" scrolling="no" style="position:static;vertical-align:top;margin:0 auto;display:block;width:860px !important;max-width:100%;min-height:433px;"></iframe>'
 })
 
 graphiq.push({
     title: 'Facebook Inc. - Top Occupations for H-1B Visa LCAs',
-    embed: '<div style="width:100%;margin:10px 0;"><iframe src="https://w.graphiq.com/w/6ay8uQGbA5n" width="600" height="400" frameborder="0" scrolling="no" style="position:static;vertical-align:top;margin:0 auto;display:block;width:600px !important;max-width:100%;min-height:400px;"></iframe><div style="text-align:center;"><a target="_blank" href="http://foreign-employment.findthedata.com/l/12658/Facebook-Inc" style="font:14px/16px arial;color:#3d3d3d;">Facebook Inc. - Top Occupations for H-1B Visa LCAs  | FindTheData</a></div></div>'
+    embed: '<iframe seamless src="https://w.graphiq.com/w/6ay8uQGbA5n" width="600" height="400" frameborder="0" scrolling="no" style="position:static;vertical-align:top;margin:0 auto;display:block;width:600px !important;max-width:100%;min-height:400px;"></iframe>'
 })
 
 graphiq.push({
     title: 'Google Inc. (GOOGL)',
-    embed: '<div style="width:100%;margin:10px 0;"><iframe src="https://w.graphiq.com/w/h9ThhRegIRv" width="700" height="522" frameborder="0" scrolling="no" style="position:static;vertical-align:top;margin:0 auto;display:block;width:700px !important;max-width:100%;min-height:522px;"></iframe><div style="text-align:center;"><a target="_blank" href="http://listings.findthecompany.com/l/8520977/Google-Inc-in-Mountain-View-CA" style="font:14px/16px arial;color:#3d3d3d;">Google Inc. (GOOGL) | FindTheCompany</a></div></div>'
+    embed: '<iframe seamless src="https://w.graphiq.com/w/h9ThhRegIRv" width="700" height="522" frameborder="0" scrolling="no" style="position:static;vertical-align:top;margin:0 auto;display:block;width:700px !important;max-width:100%;min-height:522px;"></iframe>'
 })
 
 graphiq.push({
     title: 'Google Inc.\'s Acquisitions (Since 2006)',
-    embed: '<div style="width:100%;margin:10px 0;"><iframe src="https://w.graphiq.com/w/8USVc6jHRMp" width="860" height="433" frameborder="0" scrolling="no" style="position:static;vertical-align:top;margin:0 auto;display:block;width:860px !important;max-width:100%;min-height:433px;"></iframe><div style="text-align:center;"><a target="_blank" href="http://listings.findthecompany.com/l/8520977/Google-Inc-in-Mountain-View-CA" style="font:14px/16px arial;color:#3d3d3d;">Google Inc.\'s Acquisitions (Since 2006) | FindTheCompany</a></div></div>'
+    embed: '<iframe seamless src="https://w.graphiq.com/w/8USVc6jHRMp" width="860" height="433" frameborder="0" scrolling="no" style="position:static;vertical-align:top;margin:0 auto;display:block;width:860px !important;max-width:100%;min-height:433px;"></iframe>'
 })
 
 graphiq.push({
     title: 'Google Inc. (GOOGL) Stock Price - Trailing Year',
-    embed: '<div style="width:100%;margin:10px 0;"><iframe src="https://w.graphiq.com/w/jUe3Z6RN0yx" width="600" height="487" frameborder="0" scrolling="no" style="position:static;vertical-align:top;margin:0 auto;display:block;width:600px !important;max-width:100%;min-height:487px;"></iframe><div style="text-align:center;"><a target="_blank" href="http://listings.findthecompany.com/l/8520977/Google-Inc-in-Mountain-View-CA" style="font:14px/16px arial;color:#3d3d3d;">Google Inc. (GOOGL) Stock Price - Trailing Year | FindTheCompany</a></div></div>'
+    embed: '<iframe seamless src="https://w.graphiq.com/w/jUe3Z6RN0yx" width="600" height="487" frameborder="0" scrolling="no" style="position:static;vertical-align:top;margin:0 auto;display:block;width:600px !important;max-width:100%;min-height:487px;"></iframe>'
 })
